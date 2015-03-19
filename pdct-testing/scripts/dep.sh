f10ef9dba75e1048456ec0e2143fb0478b2b99e1
Chen Tian <tianc.tech@gmail.com>
2015-03-18 21:30:12 -0700

    clean up scripts

--- pdct-testing/scripts/dep.sh
+++ pdct-testing/scripts/dep.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn install:install-file -Dfile=../core/target/tachyon-0.7.0-SNAPSHOT.jar -DgroupId=org.tachyonproject -DartifactId=tachyon -Dversion=0.7.0-SNAPSHOT-jar-with-dependencies -Dpackaging=jar -DlocalRepositoryPath=./localRepo
+mvn install:install-file -Dfile=../core/target/tachyon-0.7.0-SNAPSHOT-jar-with-dependencies.jar -DgroupId=org.tachyonproject -DartifactId=tachyon -Dversion=0.7.0-SNAPSHOT-jar-with-dependencies -Dpackaging=jar -DlocalRepositoryPath=./localRepo
